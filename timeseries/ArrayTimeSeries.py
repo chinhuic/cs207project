85b23d6a8e2e80e9c8a7a05cc389162fc3b5c086
erniechiew <ernchiew@gmail.com>
2016-10-30 16:53:30 -0400

    Modified storage for ArrayTimeSeries subclass

--- timeseries/ArrayTimeSeries.py
+++ timeseries/ArrayTimeSeries.py
@@ -1,4 +1,4 @@
-from TimeSeries_Week2 import TimeSeries # Change library name later
+from TimeSeries_Class import TimeSeries # Change library name later
 import numpy as np
 
 class ArrayTimeSeries(TimeSeries):
@@ -8,31 +8,56 @@ class ArrayTimeSeries(TimeSeries):
     
     Parameters
     ----------
+    times : sequence
+        A sequence containing the ordered time points
     values : sequence
         A sequence containing the values corresponding to the time data. 
         
     Notes
     -----
-    PRE:  data in `values` are ordered with their corresponding time, i.e.
-          1st element in `values` corresponds to 1st time point, 2nd element
-          in `values` corresponds to 2nd time point, etc.
+    PRE:
+      - `times` must be in sorted (monotonically increasing) order
+      - `times` and `values` must be of the same length
+      - data in `values` are ordered with their corresponding time, i.e.
+        1st element in `values` corresponds to 1st time point, 2nd element
+        in `values` corresponds to 2nd time point, etc.
+        
+    WARNINGS:
+      - if `times` is not sorted then indexing will be unreliable
           
     Examples
     --------
-    >>> ArrayTimeSeries([1,2,3]).value
+    >>> at1 = ArrayTimeSeries([0, 1, 2], [1, 2, 3])
+    >>> at1.value
     array([1, 2, 3])
-    
-    >>> ArrayTimeSeries(range(0,10,2)).value
-    array([0, 2, 4, 6, 8])
-    
-    >>> a1 = ArrayTimeSeries((5.1, 6.2, 3.2))
-    >>> len(a1)
+    >>> at1.time
+    array([0, 1, 2])
+    >>> len(at1)
     3
+    
+    >>> at2 = ArrayTimeSeries((0, 2, 4, 7, 10), [4, 1, 10, 2, 100])
+    >>> at2.value
+    array([  4,   1,  10,   2, 100])
+    >>> at2.time
+    array([ 0,  2,  4,  7, 10])
+    >>> len(at2)
+    5
+    
     """
     
-    def __init__(self, values):
+    def __init__(self, times, values):
+        # Check length
+        if len(times) != len(values):
+            raise ValueError('Input times and values must have the same length')
+            
+        # Check that all times are distinct 
+        # (we don't check sortedness due to time complexity)
+        if len(times) != len(set(times)):
+            raise ValueError('Input times and values must have the same length')
+        
         self.length = len([x for x in values])
         self.value = np.array([x for x in values])
+        self.time = np.array([t for t in times])
     
     def __len__(self):
         return self.length
--- timeseries/ArrayTimeSeries_tests.py
+++ timeseries/ArrayTimeSeries_tests.py
@@ -1,4 +1,3 @@
-
 from pytest import raises
 import numpy as np
 import unittest
@@ -9,22 +8,12 @@ class TestArrayTimeSeries(unittest.TestCase):
     # __init__
     # types of sequences
     def test_arg_type_range(self):
-        self.assertTrue((ArrayTimeSeries(range(0,3)).value ==
+        self.assertTrue((ArrayTimeSeries(range(0,3), range(0,3)).value ==
                          np.array([0, 1, 2])).all())
 
-    def test_arg_type_string(self):
-        self.assertTrue((ArrayTimeSeries('time').value ==
-                         np.array(['t','i','m','e'])).all())
-
     def test_arg_type_tuple(self):
-        self.assertTrue((ArrayTimeSeries((1,42,3,10)).value ==
+        self.assertTrue((ArrayTimeSeries((0, 1, 2, 4), (1,42,3,10)).value ==
                          np.array([1,42,3,10])).all())
-        
-    
-
-    def test_arg_type_list_of_tuples(self):
-        self.assertTrue((ArrayTimeSeries([(1,2),(3,4),(5,6)]).value ==
-                         np.array([(1,2),(3,4),(5,6)])).all())
         
     # number of arguments
     def test_omitted_arg(self):
@@ -34,62 +23,69 @@ class TestArrayTimeSeries(unittest.TestCase):
     def test_multiple_args(self):
         with self.assertRaises(TypeError):
             x = ArrayTimeSeries(1,2,3)
-
+        
+    # non-distinct times
+    def test_time_non_distinct(self):
+        with self.assertRaises(ValueError):
+            x = ArrayTimeSeries((1, 1, 2, 3), range(4))
+            
+    # times and values different length
+    def test_time_value_different_length(self):
+        with self.assertRaises(ValueError):
+            x = ArrayTimeSeries((1, 1, 2, 3), range(5))
+            
     # simple arguments
     def test_no_data_value(self):
-        self.assertTrue((ArrayTimeSeries([]).value == np.array([])).all())
+        self.assertTrue((ArrayTimeSeries([], []).value == np.array([])).all())
+        self.assertTrue((ArrayTimeSeries([], []).time == np.array([])).all())
 
     def test_one_data_value(self):
-        self.assertTrue((ArrayTimeSeries([1]).value == np.array([1])).all())
+        self.assertTrue((ArrayTimeSeries([0], [1]).value == np.array([1])).all())
+        self.assertTrue((ArrayTimeSeries([0], [1]).time == np.array([0])).all())
 
     def test_multiple_data_values(self):
-        self.assertTrue((ArrayTimeSeries([1,2,3]).value == np.array([1,2,3])).all())
+        self.assertTrue((ArrayTimeSeries([0,1,2], [1,2,3]).value == np.array([1,2,3])).all())
+        self.assertTrue((ArrayTimeSeries([0,1,2], [1,2,3]).time == np.array([0,1,2])).all())
 
 
     # __len__
-
     def test_length_zero(self):
-        self.assertEqual(len(ArrayTimeSeries([])), 0)
+        self.assertEqual(len(ArrayTimeSeries([],[])), 0)
 
     def test_length_nonzero(self):
-        self.assertEqual(len(ArrayTimeSeries([8,12,42])), 3)
-
-
-
+        self.assertEqual(len(ArrayTimeSeries(range(3), [8,12,42])), 3)
+    
     # __getitem__
-
     def test_getitem_index_in_series(self):
-        x = ArrayTimeSeries([2,4,6,8,10,12])
+        x = ArrayTimeSeries(range(6), [2,4,6,8,10,12])
         self.assertEqual(x[3], 8)
 
     def test_getitem_large_index_outside_boundary(self):
-        x = ArrayTimeSeries([2,4,6,8,10,12])
+        x = ArrayTimeSeries(range(6), [2,4,6,8,10,12])
         with self.assertRaises(IndexError):
             x[10]
 
     def test_getitem_negative_index(self):
-        x = ArrayTimeSeries([2,4,6,8,10,12])
+        x = ArrayTimeSeries(range(6), [2,4,6,8,10,12])
         self.assertEqual(x[-1], 12)
-
-
-
-
+        
     # __setitem__
     def test_setitem(self):
-        x = ArrayTimeSeries([2,4,6,8,10,12])
+        x = ArrayTimeSeries(range(6), [2,4,6,8,10,12])
         x[2] = 7
         self.assertTrue((x.value == np.array([2,4,7,8,10,12])).all())
-    
+        self.assertTrue((x.time == np.array(range(6))).all())
+        
     # __iter__ 
     # Test iterating over ATS (ArrayTimeSeries) with empty values
     def test_iter_empty(self):
-        ats_empty = ArrayTimeSeries([])
+        ats_empty = ArrayTimeSeries([], [])
         with self.assertRaises(StopIteration): 
             next(iter(ats_empty))
             
     # Test iterating over ATS with one value
     def test_iter_simple(self):
-        ats_simple = ArrayTimeSeries([1])
+        ats_simple = ArrayTimeSeries([0], [1])
         ats_simple_iter = iter(ats_simple)
         self.assertEqual(ats_simple.value[0], next(ats_simple_iter))
         with self.assertRaises(StopIteration): 
@@ -98,7 +94,7 @@ class TestArrayTimeSeries(unittest.TestCase):
     # Test iterating over ATS with evenly spaced integer values
     # Check both the types of results and answers expected
     def test_iter_integer_spaced_1(self):
-        ats = ArrayTimeSeries(range(15))
+        ats = ArrayTimeSeries(range(15), range(15))
         
         iter_list = []
         for val in ats:
@@ -111,7 +107,7 @@ class TestArrayTimeSeries(unittest.TestCase):
         self.assertTrue((iter_list == ats.value).all())
         
     def test_iter_integer_spaced_3(self):
-        ats = ArrayTimeSeries(range(-5, 17, 3))
+        ats = ArrayTimeSeries(range(-5, 17, 3), range(-5, 17, 3))
         
         iter_list = []
         for val in ats:
@@ -124,7 +120,7 @@ class TestArrayTimeSeries(unittest.TestCase):
         self.assertTrue((iter_list == ats.value).all())
         
     def test_iter_integer_spaced_7(self):
-        ats = ArrayTimeSeries(range(-14, 701, 7))
+        ats = ArrayTimeSeries(range(-14, 701, 7), range(-14, 701, 7))
         
         iter_list = []
         for val in ats:
@@ -139,7 +135,7 @@ class TestArrayTimeSeries(unittest.TestCase):
     # Test iterating over a ATS with values of type float
     # Check both the types of results and answers expected
     def test_iter_float_values(self):
-        ats = ArrayTimeSeries((3.14159, 42., -0.207))
+        ats = ArrayTimeSeries((0.01, 0.5, 5.3), (3.14159, 42., -0.207))
         
         iter_list = []
         for val in ats:
@@ -154,29 +150,26 @@ class TestArrayTimeSeries(unittest.TestCase):
     # itertimes
     # Test itertimes method over ATS with empty values
     def test_itertimes_empty(self):
-        ats_empty = ArrayTimeSeries([])
+        ats_empty = ArrayTimeSeries([], [])
         with self.assertRaises(StopIteration): 
             next(ats_empty.itertimes())
             
     # Test itertimes method over ATS with one value
     def test_itertimes_simple(self):
-        ats_simple = ArrayTimeSeries([42])
+        ats_simple = ArrayTimeSeries([3], [42])
         ats_simple_itertimes = ats_simple.itertimes()
-        self.assertEqual(0, next(ats_simple_itertimes))
+        self.assertEqual(3, next(ats_simple_itertimes))
         with self.assertRaises(StopIteration): 
             next(ats_simple_itertimes)
             
     # Test iterating over ATS with integers
     # Check both the types of results and answers expected
     def test_itertimes_integer(self):
-        ats = ArrayTimeSeries([1, 5, 3, 6])
+        ats = ArrayTimeSeries(range(4), [1, 5, 3, 6])
         
         iter_list = []
         for val in ats.itertimes():
             iter_list.append(val)
-            
-        # Check type
-        self.assertTrue(all(isinstance(n, int) for n in iter_list))
         
         # Check results
         self.assertEqual([0, 1, 2, 3], iter_list)
@@ -184,22 +177,22 @@ class TestArrayTimeSeries(unittest.TestCase):
     # iteritems
     # Test iteritems method over ATS with empty values
     def test_iteritems_empty(self):
-        ats_empty = ArrayTimeSeries([])
+        ats_empty = ArrayTimeSeries([], [])
         with self.assertRaises(StopIteration): 
             next(ats_empty.iteritems())
             
     # Test itertimes method over ATS with one value
     def test_iteritems_simple(self):
-        ats_simple = ArrayTimeSeries([42])
+        ats_simple = ArrayTimeSeries([3], [42])
         ats_simple_iteritems = ats_simple.iteritems()
-        self.assertEqual((0, 42), next(ats_simple_iteritems))
+        self.assertEqual((3, 42), next(ats_simple_iteritems))
         with self.assertRaises(StopIteration): 
             next(ats_simple_iteritems)
             
     # Test iterating over a ATS with integers
     # Check both the types of results and answers expected
     def test_iteritems_integer(self):
-        ats = ArrayTimeSeries([1, 5, 3, 6])
+        ats = ArrayTimeSeries(range(4), [1, 5, 3, 6])
         
         iter_list = []
         for val in ats.iteritems():
