b22be339e3b533654b836eb99b449962a97ef06f
nripsuta <nripsuta@users.noreply.github.com>
2016-11-01 23:41:59 -0400

    Update StreamTimeSeriesInterface.py

--- timeseries/StreamTimeSeriesInterface.py
+++ timeseries/StreamTimeSeriesInterface.py
@@ -1,12 +1,28 @@
 import abc
-import random
-import collections.abc
+
 class StreamTimeSeriesInterface(abc.ABC):
 
+    """
+    An ABC (abstract base class) or interface to handle those timeseries that do not
+    have an underlying array (or storage). 
+    
+    """
     @abc.abstractmethod
     def produce(self, chunk = 1):
         """
+        An abstract (generator) method that generates a chunk sized bunch
+        of new elements into the timeseries whenever it is called
+
+        PARAMETER
+        ----------
+        chunk: int
+            the chunk argument must be an integer; default value is 1
+
+        YIELDS
+        -------
+        the chunk-sized list of values.  
         """
         #yield random.sample(range(1, 10000000), chunk)
         
         
+
