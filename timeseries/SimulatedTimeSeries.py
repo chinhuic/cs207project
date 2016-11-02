2168d4d567540e640bb3248ce1b5bfe38c29e45a
nripsuta <nripsuta@users.noreply.github.com>
2016-11-01 23:42:55 -0400

    Update SimulatedTimeSeries.py

--- timeseries/SimulatedTimeSeries.py
+++ timeseries/SimulatedTimeSeries.py
@@ -1,15 +1,35 @@
-from StreamTimeSeriesInterface import StreamTimeSeriesInterface
+ from StreamTimeSeriesInterface import StreamTimeSeriesInterface
 
 class SimulatedTimeSeries(StreamTimeSeriesInterface):
+    """
+    SimulatedTimeSeries class
+     
+    Construction:  ts = SimulatedTimeSeries(<any generator function>)
+
+    PRE: argument passed to constructor must be a generator
+    
+    """
+
 
      def __init__(self, generator_method):
+         """
+         Constructor for SimulatedTimeSeries. Argument is generator_method: a generator method, which
+         is then bound to self._gmethod
+         """
          
-         self._gmethod = generator_method
+         self._gmethod = generator_method()
          
           
 
     
      def produce(self, chunk = 1):
+         """
+         Inherited from StreamTimeSeriesInterface. Uses the generator method passed as an argument to the
+         constructor, which was bound to self._gmethod, to generate a sequence with chunk number of values. It then
+         yields that sequence. 
+
+        
+         """
          a_list = []
 
          g = self._gmethod
