257310fc13c38b563870b863c87971ace0a0b725
erniechiew <ernchiew@gmail.com>
2016-11-02 22:12:33 -0400

    Fixed error message in __init__

--- timeseries/TimeSeries.py
+++ timeseries/TimeSeries.py
@@ -70,7 +70,7 @@ class TimeSeries(SizedContainerTimeSeriesInterface):
         # Check that all times are distinct 
         # (we don't check sortedness due to time complexity)
         if len(times) != len(set(times)):
-            raise ValueError('Input times and values must have the same length')
+            raise ValueError('Input times must be distinct!')
             
         # Check if input data is numeric
         if not all(isinstance(x, numbers.Number) for x in values):
