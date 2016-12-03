e9747a83ebe70e48c44ad9eee4bd71baabfcd266
erniechiew <ernchiew@gmail.com>
2016-12-02 22:00:19 -0500

    Fixed rbt test file

--- timeseries/test_cs207rbtree.py
+++ timeseries/test_cs207rbtree.py
@@ -1,7 +1,7 @@
 import unittest
 from cs207rbtree import *
 
-class TestCS207BST(unittest.TestCase):
+class TestCS207RBT(unittest.TestCase):
 	# input types
 	def test_string_key_and_string_value(self):
 		db = connect("/tmp/test.dbdb")
@@ -10,7 +10,9 @@ class TestCS207BST(unittest.TestCase):
 		db.close()
 		db = connect("/tmp/test.dbdb")
 
+      
 		self.assertEqual(db.get('a'),'apple')
+		
 
 		db.close()
 		os.remove("/tmp/test.dbdb")
@@ -22,6 +24,7 @@ class TestCS207BST(unittest.TestCase):
 		db.close()
 		db = connect("/tmp/test.dbdb")
 
+    
 		self.assertEqual(db.get(5),42)
 
 		db.close()
@@ -34,6 +37,7 @@ class TestCS207BST(unittest.TestCase):
 		db.close()
 		db = connect("/tmp/test.dbdb")
 
+     
 		self.assertEqual(db.get(5.7),42.10)
 
 		db.close()
@@ -46,6 +50,7 @@ class TestCS207BST(unittest.TestCase):
 		db.close()
 		db = connect("/tmp/test.dbdb")
 
+           
 		self.assertEqual(db.get('a'),42)
 
 		db.close()
@@ -65,6 +70,8 @@ class TestCS207BST(unittest.TestCase):
 
 	def test_tuple_key(self):
 		db = connect("/tmp/test.dbdb")
+		
+
 		with self.assertRaises(TypeError):
 			db.set((1,2),5)
 		db.close()
@@ -78,7 +85,8 @@ class TestCS207BST(unittest.TestCase):
 		db.commit()
 		db.close()
 		db = connect("/tmp/test.dbdb")
-
+		
+   
 		self.assertEqual(db.get('20'),42)
 
 		db.close()
@@ -91,6 +99,7 @@ class TestCS207BST(unittest.TestCase):
 		db.close()
 		db = connect("/tmp/test.dbdb")
 
+     
 		self.assertTrue(type(db.get(5))==int)
 		self.assertEqual(str(db.get(5)),'42')
 
@@ -99,6 +108,8 @@ class TestCS207BST(unittest.TestCase):
 
 	def test_tuple_value(self):
 		db = connect("/tmp/test.dbdb")
+		
+	
 		with self.assertRaises(TypeError):
 			db.set(5,(1,2))
 		db.close()
@@ -110,15 +121,71 @@ class TestCS207BST(unittest.TestCase):
 		db.set('a','apple')
 		db.close()
 		db = connect("/tmp/test.dbdb")
+
+		
+		with self.assertRaises(KeyError):
+			db.get('a')
+		db.close()
+		os.remove("/tmp/test.dbdb")
+	
+	def test_get_without_inserting(self):
+		db = connect("/tmp/test.dbdb")
+		# db.set('a','apple')
+		db.close()
+		db = connect("/tmp/test.dbdb")
+
+
 		with self.assertRaises(KeyError):
 			db.get('a')
 		db.close()
 		os.remove("/tmp/test.dbdb")
 
-	def test_different_node_types(self):
-		pass
+	def test_get_parent(self):
+		db = connect("/tmp/test.dbdb")
+		db.set(0,'a')
+		db.set(5.7,42.10)
+		db.set(5,'apple')
+		db.set(4,82)
+		db.set(97,42)
+		db.commit()
+		db.close()
+		db = connect("/tmp/test.dbdb")
 
+
+		# using get_parent to check if the nodes have the correct parents
+		parent, side = db._tree.get_parent(0)
+		self.assertEqual(parent.key,5)
+		self.assertEqual(side,'L')	
+		parent, side = db._tree.get_parent(97)
+		self.assertEqual(parent.key,5.7)
+		self.assertEqual(side,'R')
+		db.close()
+		os.remove("/tmp/test.dbdb")
+
+	def test_get_parent_of_root(self):
+		db = connect("/tmp/test.dbdb")
+		db.set(0,'a')
+		db.set(1,'b')
+		db.set(2,'c')
+		db.set(3,'d')
+		db.set(4,'e')
+		db.set(5,'f')
+		db.commit()
+		db.close()
+		
+		db = connect("/tmp/test.dbdb")
+		parent = db._tree.get_parent(1)
+		db.close()
+		os.remove("/tmp/test.dbdb")
+
+		
 	def test_one_balancing(self):
+		# one rotation at a time
+		# creates the tree
+		# without balancing, 2 should have been the parent of 3
+		# after balancing, 1 is now the parent of 3
+
+
 		db = connect("/tmp/test.dbdb")
 		db.set(0,'a')
 		db.set(1,'b')
@@ -140,6 +207,44 @@ class TestCS207BST(unittest.TestCase):
 		os.remove("/tmp/test.dbdb")
 
 	def test_two_balancing(self):
+		# one rotation directly followed by another rotation
+		# creates the tree
+		# without balancing, 7 should have been the parent of 18, and 18 should have been the parent of 10
+		# after balancing, 10 is now the parent of 7 and 18
+		db = connect("/tmp/test.dbdb")
+		db.set(7,'x')
+		db.set(3,1)
+		db.set(18,'a')
+		db.set(10,'z')
+		db.set(22,'hello')
+		db.set(8,'x')
+		db.set(11,'m')
+		db.set(26,'p')
+		db.set(12,23)
+		db.commit()
+		db.close()
+
+		db = connect("/tmp/test.dbdb")
+
+		# using get_parent to check if tree has been balanced correctly
+		parent, side = db._tree.get_parent(7)
+		# checking that 7 is now the left child of 10
+		self.assertEqual(parent.key,10)
+		self.assertEqual(side,'L')
+		#self.assertEqual(str(side),'L')	
+
+		# using get_parent to check if tree has been balanced correctly
+		parent, side = db._tree.get_parent(18)
+		# checking that 18 is now the right child of 10
+		self.assertEqual(parent.key,10)
+		self.assertEqual(side,'R')
+		#self.assertEqual(str(side),'R')
+		
+		db.close()
+		os.remove("/tmp/test.dbdb")               
+	
+
+	def test_balance_after_update(self):
 		db = connect("/tmp/test.dbdb")
 		db.set(7,'x')
 		db.set(3,'x')
@@ -155,15 +260,74 @@ class TestCS207BST(unittest.TestCase):
 
 		db = connect("/tmp/test.dbdb")
 
+		db.set(27,'j')
+
+		# after balancing, parent of 22 should change from 18 to 26
 		# using get_parent to check if tree has been balanced correctly
-		parent, side = db._tree.get_parent(7)
-		# checking that 7 is now the left child of 10
-		self.assertEqual(parent.key,10)
+		parent, side = db._tree.get_parent(22)
+		# checking that 22 is now the left child of 26
+		self.assertEqual(parent.key,26)
 		self.assertEqual(side,'L')
 		#self.assertEqual(str(side),'L')	
 
+		# after balancing, 26 should be the right child of 18
+		# using get_parent to check if tree has been balanced correctly
+		parent, side = db._tree.get_parent(26)
+		# checking that 26 is now the right child of 18
+		self.assertEqual(parent.key,18)
+		self.assertEqual(side,'R')
+		#self.assertEqual(str(side),'R')
+		
 		db.close()
 		os.remove("/tmp/test.dbdb")
 
+
+	def test_delete(self):
+		db = connect("/tmp/test.dbdb")
+		db.set(0,'a')
+		db.set(1,'b')
+		db.set(2,'c')
+		db.set(3,'d')
+		db.set(4,'e')
+		db.set(5,'f')
+		db.commit()
+		db.close()
+		db = connect("/tmp/test.dbdb")
+
+		db.delete(4)
+
+		# using get_parent to check if the nodes have the correct parents
+		parent, side = db._tree.get_parent(5)
+		self.assertEqual(parent.key,3)
+		self.assertEqual(side,'R')	
+		db.close()
+		os.remove("/tmp/test.dbdb")
+
+	def test_delete_and_rebalance(self):
+		db = connect("/tmp/test.dbdb")
+		db.set(0,'a')
+		db.set(1,'b')
+		db.set(2,'c')
+		db.set(3,'d')
+		db.set(4,'e')
+		db.set(5,'f')
+		db.commit()
+		db.close()
+		db = connect("/tmp/test.dbdb")
+
+		db.delete(2)
+
+		# using get_parent to check if the nodes have the correct parents
+		parent, side = db._tree.get_parent(5)
+		self.assertEqual(parent.key,4)
+		self.assertEqual(side,'R')
+		parent, side = db._tree.get_parent(0)
+		self.assertEqual(parent.key,1)
+		self.assertEqual(side,'L')
+		
+		db.close()
+		os.remove("/tmp/test.dbdb")
+
+
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
